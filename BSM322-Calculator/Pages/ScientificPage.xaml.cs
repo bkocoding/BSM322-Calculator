b768fd6ea99fd3f49ced1ff2e9ae24e17db20fd1
Burak Kaan ORAK <burakkaanorak@gmail.com>
2025-10-13 00:14:54 +0300

    Refactor ScientificPage layout and add calculator logic
    Restructured `ScientificPage.xaml` to use a grid layout for improved usability, adding input fields and buttons for a scientific calculator. Introduced `CalculatorService` in `ScientificPage.xaml.cs` to handle button click events and added methods for number, operator, and memory operations, including angle mode toggling.
    
    This page still requires to add 2nd button functionality and CalculatorService needs to implement new scientific functions.

--- BSM322-Calculator/Pages/ScientificPage.xaml.cs
+++ BSM322-Calculator/Pages/ScientificPage.xaml.cs
@@ -1,9 +1,52 @@
+using BSM322_Calculator.Services;
+
 namespace BSM322_Calculator.Pages;
 
 public partial class ScientificPage : ContentPage
 {
-	public ScientificPage()
+    private readonly CalculatorService calculator;
+
+    public ScientificPage()
 	{
 		InitializeComponent();
-	}
+        calculator = new CalculatorService(OperationEntry, ResultEntry);
+    }
+
+    private void NumberClickEvent(object sender, EventArgs e)
+    {
+        calculator.NumberClickEvent(sender, e);
+    }
+
+    private void OperatorClickEvent(object sender, EventArgs e)
+    {
+        calculator.OperatorClickEvent(sender, e);
+    }
+
+    private void MemoryClickEvent(object sender, EventArgs e)
+    {
+        calculator.MemoryClickEvent(sender, e);
+    }
+
+    private void DegClickEvent(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        if (button.Text == "DEG")
+        {
+            button.Text = "RAD";
+
+        }
+        else if (button.Text == "RAD")
+        {
+            button.Text = "GRAD";
+        }
+        else if (button.Text == "GRAD")
+        {
+            button.Text = "DEG";
+        }
+    }
+
+    private void FEClickEvent(object sender, EventArgs e)
+    {
+
+    }
 }
